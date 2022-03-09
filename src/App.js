28c92fedac49b57588385ae3e017c8a3c47eeaa9
SnakeBearLB <79704111+SnakeBearLB@users.noreply.github.com>
2022-03-09 17:12:20 -0500

    initial setup of ffmpeg.wasm

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {useEffect, useState} from 'react'
+import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg';
+const ffmpeg = createFFmpeg({ log: true });
 
 function App() {
+  const [ready, setReady] = useState(false);
+  const [video, setVideo] = useState();
+
+  const load = async () => {
+    await ffmpeg.load();
+    setReady(true);
+  }
+
+  useEffect(() => {
+    load();
+  }, [])
+
+  // SEQUENCE add audio
+    // GET audio from 
+  // add audio
+  // extend last frame of video for length of audio
+  // merge video files
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
